19ae5db804c59b58ac4cc82cb067f093480fefd3
eonduck2 <whdtnxd@gmail.com>
2024-06-12 16:40:12 +0900

    update files

--- work-4.js
+++ work-4.js
@@ -3,13 +3,21 @@ const tagComponent = (tagName, textNode) => {
 };
 
 const container = () => {
-  const dateObj = new Date();
-  console.log(dateObj.getTime());
-  return `
+  const dateObj = new Date().getHours();
+
+  if (dateObj < 12) {
+    return `
       ${tagComponent("header", "헤더부분")}
-      ${tagComponent("main", "메인부분")}
+      ${tagComponent("div", "AM")}
       ${tagComponent("footer", "푸터부분")}
     `;
+  } else {
+    return `
+      ${tagComponent("header", "헤더부분")}
+      ${tagComponent("div", "PM")}
+      ${tagComponent("footer", "푸터부분")}
+    `;
+  }
 };
 
 container();
